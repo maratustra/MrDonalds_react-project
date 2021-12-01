fbe26a34ef91a53e1043061ce25ddb3a5fa79ccd
Alena Maramygina <maratustraa@gmail.com>
2021-12-01 20:43:26 +0500

    feat(DBMenu.js, GlobalStyle.js, ListItem.js, Menu.js): added new components

---
+++ src/Components/GlobalStyle.js
@@ -0,0 +1,52 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *,
+  *::after,
+  *::before {
+    box-sizing: inherit;
+  }
+  
+  body {
+	  margin: 0;
+    background-color: #f0f0f0;
+	  color: black;
+    font-family: Roboto, sans-serif;
+    font-size: 20px;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  input, button {
+    font-family: inherit;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+    cursor: pointer;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  h1, h2, h3 {
+    margin: 0;
+    padding: 0;
+    font-family: Pacifico;
+  }
+`;
