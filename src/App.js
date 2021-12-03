fdae917ca6fe4fa784935df02f8339f3b3e70ff3
Alena Maramygina <maratustraa@gmail.com>
2021-12-03 15:05:23 +0500

    feat(Hooks/useOpenItem.js): added new folder for hooks and new hook

--- src/App.js
+++ src/App.js
@@ -4,18 +4,19 @@ import { Menu } from './Components/Menu';
 import { GlobalStyle } from './Components/GlobalStyle';
 import { ModalItem } from './Components/ModalItem';
 import { Order } from './Components/Order';
+import { useOpenItem } from './Components/Hooks/useOpenItem';
 
 function App() {
 
-  const [openItem, setOpenItem] = React.useState(null);
+  const openItem = useOpenItem();
 
   return (
     <>
       <GlobalStyle />
       <NavBar />
       <Order />
-      <Menu setOpenItem={setOpenItem} />
-      <ModalItem openItem={openItem} setOpenItem={setOpenItem} />
+      <Menu {...openItem} />
+      <ModalItem {...openItem} />
     </>
   );
 }
